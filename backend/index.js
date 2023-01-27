beacba596068505673b73d9eacb3b2cf57450c40
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-01-27 13:50:06 +0100

    basic frontend ui structure

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,12 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
+app.use("/services/auth", authRoute);
+app.use("/services/users", userRoute);
+app.use("/services/products", productRoute);
+app.use("/services/carts", cartRoute);
+app.use("/services/orders", orderRoute);
+app.use("/services/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
